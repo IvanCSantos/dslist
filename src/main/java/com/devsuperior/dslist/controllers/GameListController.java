5202608727db9328fdd4e4a45b4e27d9c5405e23
Ivan Santos <ivancsantos@hotmail.com.br>
2025-05-20 20:24:41 -0300

    Game List Projection

--- src/main/java/com/devsuperior/dslist/controllers/GameListController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameListController.java
@@ -1,7 +1,9 @@
 package com.devsuperior.dslist.controllers;
 
 import com.devsuperior.dslist.dto.GameListDTO;
+import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.services.GameListService;
+import com.devsuperior.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +18,8 @@ public class GameListController {
 
   @Autowired
   private GameListService gameListService;
+  @Autowired
+  private GameService gameService;
 
   @GetMapping(value = "/{id}")
   public GameListDTO findById(@PathVariable Long id) {
@@ -26,6 +30,12 @@ public class GameListController {
   @GetMapping
   public List<GameListDTO> findAll() {
     List<GameListDTO> result = gameListService.findAll();
+    return result;
+  }
+
+  @GetMapping(value = "/{listId}/games")
+  public List<GameMinDTO> findByList(@PathVariable Long listId) {
+    List<GameMinDTO> result = gameService.findByList(listId);
     return result;
   }
 
